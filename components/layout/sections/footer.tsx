28649fc8b567c17ca3fa847b2f24ca1bdb24b7ae
keya161 <kkesani@gmail.com>
2025-03-25 13:09:28 +0530

    upload files

---
+++ components/layout/sections/footer.tsx
@@ -0,0 +1,9 @@
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+import Image from "next/image";
+
+export const FooterSection = () => {
+  return (
+    <></>
+  );
+};
